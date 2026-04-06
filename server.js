4bb0430c6ddf0c8fc5fb21e8e8a6f4bc03a84e27
Muhammed Shamil T P <130552723+muhammedshamil313@users.noreply.github.com>
2026-04-06 12:18:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // ================= DATABASE =================
@@ -57,6 +59,8 @@ app.delete("/tasks/:id", async (req, res) => {
 });
 
 // ================= SERVER =================
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
